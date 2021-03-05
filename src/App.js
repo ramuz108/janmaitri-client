bc7693820cffb61f7673d573780caeb4de4bc911
ramuz108 <ramhero22@gmail.com>
2021-03-05 20:16:03 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component ,useEffect, useState  }  from "react";
+import clientbg from './static/clientbg.gif';
+import { withSnackbar } from 'notistack';
+import axios from 'axios';
+class App extends React.Component
+{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+
+    super(props);
+  
+    this.state = { apiResponse: [],info: 0,error: 0,lat: 0,lon: 0,ip: "",db: "",date: "", offset: 0,tableData: [],orgtableData: [], perPage: 2,currentPage: 0,weatherloc: "",temp: 0,weatherdesc: "",weathericon: "",prevdata:0,intrusion:[],movement:[],vibration:[],heat:[],light:[]}
+    
+  }
+  componentDidMount(){
+    this.init();
+    setInterval(() => {
+      this.alarmdata();
+      this.getlength();
+    }, (3000));
+  }
+  init = () =>{
+    axios
+    .get('https://janamaitri.herokuapp.com/alm')
+    .then(res => {
+      
+        this.setState({
+            prevdata : res.data.results.length      
+        })
+    });
+  }
+  displaymove = (zone) =>{
+  
+    this.props.enqueueSnackbar('Movement Detected at '+zone, { 
+      variant: 'warning',
+      autoHideDuration: 2000,
+      anchorOrigin: {
+        vertical: 'bottom',
+        horizontal: 'center',
+        
+    },
+   });
+  }
+  getlength = () =>
+{ 
+ 
+  //console.log(this.state.orgtableData[0]);
+  console.log("Data: "+ this.state.orgtableData.length);
+  console.log("Previous Data: "+ this.state.prevdata);
+  if(this.state.orgtableData.length > this.state.prevdata){
+    var remaining = this.state.orgtableData.length - this.state.prevdata;
+    var balance = this.state.orgtableData.length - remaining;
+    for(var i = 0; i < remaining; i++){
+      console.log(this.state.orgtableData[i]);
+      
+          this.displaymove("SN - P");
+       
+    }
+    //console.log("Move-->"+this.state.movement[0].alert_lat);
+   // this.setState({prevdata: this.orgtableData.length});
+  }
+}
+alarmdata = () =>{
+
+  axios
+  .get('https://janamaitri.herokuapp.com/alm')
+  .then(res => {
+      var tdata = res.data.results;
+     console.log('data-->'+JSON.stringify(tdata))
+     
+      this.setState({
+         
+          orgtableData : tdata,
+          
+      })
+  });
+}
+  render(){
+    return(
+
+      <div style={{backgroundColor:"black",width:"100%",height:"100%"}}>
+          <center>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <img src={clientbg} width="500" height="250" />
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          </center>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withSnackbar(App);
